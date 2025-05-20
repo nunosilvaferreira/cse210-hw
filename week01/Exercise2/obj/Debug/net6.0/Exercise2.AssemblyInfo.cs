d744c7a567c3cf3720b96720d331814cd68fff27
nunosilvaferreira <nferreira@byupathway.edu>
2025-05-20 16:12:24 +0100

    W03.2_Encapsulation_Explanation

--- week01/Exercise2/obj/Debug/net6.0/Exercise2.AssemblyInfo.cs
+++ week01/Exercise2/obj/Debug/net6.0/Exercise2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f27e1d530c2ec32e9532b56bdcf4b3aed545055")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeb111c0dcb24e6539460406395542466f180ff4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
