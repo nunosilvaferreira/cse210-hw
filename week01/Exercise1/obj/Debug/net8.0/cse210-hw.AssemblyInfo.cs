4b87781b3bb13d5d2cf78a128de82ce687adbbf3
nunosilvaferreira <nferreira@byupathway.edu>
2025-06-04 15:18:01 +0100

    W05.1.2

--- week01/Exercise1/obj/Debug/net8.0/cse210-hw.AssemblyInfo.cs
+++ week01/Exercise1/obj/Debug/net8.0/cse210-hw.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cse210-hw")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0750ddb0aad507d8e6b65f4a97c1ea8911b5f101")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c07c17005b420f3085100f48905bae363ab58901")]
 [assembly: System.Reflection.AssemblyProductAttribute("cse210-hw")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cse210-hw")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- week01/Exercise2/obj/Debug/net6.0/Exercise2.AssemblyInfo.cs
+++ week01/Exercise2/obj/Debug/net6.0/Exercise2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0750ddb0aad507d8e6b65f4a97c1ea8911b5f101")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c07c17005b420f3085100f48905bae363ab58901")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
